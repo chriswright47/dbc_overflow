86bf28e25e9f8886f6c483ee68fd5f32e271a166
Chris Wright <christopher.wright47@gmail.com>
2013-11-20 15:54:12 -0600

    commiting empty rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DBCOverflow::Application.config.secret_token = '133efca7b32f398c07c42e5f7f0eb5ba9e4dc34836be0ed3540c85fe31dcaf510d2741125bee16481f143fd966cb6fb3065af6a6e207f594efb43b4f8fce76bd'
