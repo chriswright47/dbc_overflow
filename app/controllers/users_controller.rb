7cd6d8c82abbaaf2c40a0a915db445a84444200a
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2013-11-21 16:21:54 -0600

    Added sessions feature, not yet tested

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,4 +30,8 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all
+  end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+  def signed_in?
+    !session[:user_id].nil?
+  end
+
+  def current_user
+    User.find(session[:user_id])
+  end
 end
