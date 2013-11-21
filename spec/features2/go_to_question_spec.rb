d8d9439f652d881ec3f5431a0561b9d880a2a324
Chris Wright <christopher.wright47@gmail.com>
2013-11-21 14:54:21 -0600

    pulled in feature tests from previous branch

--- spec/features2/go_to_question_spec.rb
+++ spec/features2/go_to_question_spec.rb
@@ -1,27 +1,48 @@
 require 'spec_helper'
 
+# feature "A user can create a question" do
+#   scenario "a particular user" do
+#     visit 'users'
+#     click_link 'Sign Up'
+#     fill_in "username", :with => "Chris"
+#     fill_in "email", :with => "chris@mail.com"
+#     fill_in "phase", :with => 3
+#     fill_in "password", :with => "password"
+#     click_button "Create User"
+#     click_link 'Create a New Question'
+#     fill_in "title", :with => "Why can't Alex code"
+#     fill_in "body", :with => "Because he just can't."
+#     click_button "Create Question"
+#     # question = Question.create(title: 'Title', body: 'body', user_id: user.id)
+#     # thing = mock(question, :title => 'Title')
+#     # visit "/questions"
+#     # # save_and_open_page
+#     # click_link "#{thing.title}"
+#     expect(page).to have_text("Why can't Alex code")
+#     expect(page).to have_text("Because he just can't.")
+#     expect(page).to have_text("Chris")
+# end
+# end
+
+# feature "A user can click on a question and go to the question" do
+#   scenario "a particular question" do
+#     # visit "/questions"
+#     # click_link
+#     end
+# end
+
 feature "A user can create a question" do
   scenario "a particular user" do
-    visit 'users'
-    click_link 'Sign Up'
-    fill_in "username", :with => "Chris"
-    fill_in "email", :with => "chris@mail.com"
-    fill_in "phase", :with => 3
-    fill_in "password", :with => "password"
-    click_button "Create User"
-    click_link 'Create a New Question'
+    user = FactoryGirl.create(:user)
+    visit "/questions"
+    click_link "Create a New Question"
+    save_and_open_page
     fill_in "title", :with => "Why can't Alex code"
     fill_in "body", :with => "Because he just can't."
     click_button "Create Question"
-    # question = Question.create(title: 'Title', body: 'body', user_id: user.id)
-    # thing = mock(question, :title => 'Title')
-    # visit "/questions"
-    # # save_and_open_page
-    # click_link "#{thing.title}"
     expect(page).to have_text("Why can't Alex code")
     expect(page).to have_text("Because he just can't.")
     expect(page).to have_text("Chris")
+
   end
-
-
 end
