4d7b0f3cdc954df1d2ee466aa06a8a037a0973dd
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2013-11-21 20:30:42 -0600

    Moved session helpers into application helper and controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def signed_in?
+    !session[:user_id].nil?
+  end
+
+  def current_user
+  	if signed_in?
+	    User.find(session[:user_id])
+	  else
+	  	nil
+	  end
+  end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,2 @@
 module SessionsHelper
-  def signed_in?
-    !session[:user_id].nil?
-  end
-
-  def current_user
-    User.find(session[:user_id])
-  end
 end
