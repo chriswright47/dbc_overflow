2815ced0b23d4e295b2ef2e581dbfda7e6068cfe
connor McCrone <your.email@example.com>
2013-11-22 15:18:44 -0600

    Added user delete account feature and removed logout link from users index

---
+++ spec/features/delete_user_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+feature "A user should be able to delete their account" do
+	scenario "when signed in" do
+		user = FactoryGirl.create(:user)
+		visit "/"
+		click_link 'Sign In'
+		fill_in 'session_email', :with => user.email
+    fill_in 'session_password', :with => user.password
+    click_button 'Sign In'
+    click_link 'Users'
+    expect(page).to have_text("#{user.username}")
+    click_link "My Profile"
+    click_button "Delete Account"
+    expect(page).to_not have_text("My Profile")
+    expect(page).to_not have_text("Sign Out")
+    expect(page).to have_text("Sign Up")
+    expect(page).to have_text("Sign In")
+    click_link 'Users'
+    expect(page).to_not have_text("#{user.username}")
+	end
+end
