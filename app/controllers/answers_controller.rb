892a2d9cfab9ec93798b5cbefc2c2aca0dc04a1e
connor McCrone <your.email@example.com>
2013-11-22 13:42:03 -0600

    edit answer feature test and added answer factory

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -11,4 +11,16 @@ class AnswersController < ApplicationController
 		question.answers << answer 
 		redirect_to question_path(question)
 	end
+
+	def edit 
+		@answer = Answer.find(params[:id])
+		@question = @answer.question
+	end
+
+	def update
+		question = Question.find(params[:question_id])
+		answer = Answer.find(params[:id])
+		answer.update_attributes(params[:answer])
+		redirect_to question_path(question)
+	end
 end
---
+++ spec/features/edit_answer_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+feature "A user can edit an answer" do 
+	scenario "when a user is the author of an answer" do 
+		user = FactoryGirl.create(:user)
+		question = FactoryGirl.create(:question, :user_id => user.id)
+		answer = FactoryGirl.create(:answer, :user_id => user.id, :question_id => question.id)
+		visit question_path(question)
+		click_link 'Edit Answer'
+		expect(page).to have_text(answer.body)
+		fill_in "answer_body", :with => "Here is the new answer to your question"
+		click_button "Update Answer"
+		expect(current_path).to eq question_path(question)
+		expect(page).to have_text("Here is the new answer to your question")
+	end
+end
