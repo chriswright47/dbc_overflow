4df9b5eb75fdd36746153eb845e233eaa9ac4920
Chris Wright <christopher.wright47@gmail.com>
2013-11-22 07:42:33 -0600

    fixed our question integration tests so that they work

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,7 +3,7 @@ require 'factory_girl_rails'
 FactoryGirl.define do
   factory :user do
     username "Chris"
-    email "Chris@mail.com"
+    email "chris@mail.com"
     phase 3
     password "1234"
   end
