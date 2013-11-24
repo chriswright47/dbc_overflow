80fd7b595dca9a9892c13b7284603d1d5767af0d
Chris Wright <christopher.wright47@gmail.com>
2013-11-24 14:35:06 -0600

    fixed git issue so styles and seeds exist w/o ajax stuff

--- db/migrate/20131120215736_create_questions.rb
+++ db/migrate/20131120215736_create_questions.rb
@@ -3,7 +3,7 @@ class CreateQuestions < ActiveRecord::Migration
     create_table :questions do |t|
       t.integer :user_id
       t.string :title
-      t.string :body
+      t.text :body
 
       t.timestamps
     end
--- db/migrate/20131120215740_create_answers.rb
+++ db/migrate/20131120215740_create_answers.rb
@@ -3,7 +3,7 @@ class CreateAnswers < ActiveRecord::Migration
     create_table :answers do |t|
       t.integer :user_id
       t.integer :question_id
-      t.string :body
+      t.text :body
 
       t.timestamps
     end
