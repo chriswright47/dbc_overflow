00a23ab99233aea4899693f2a0055968db7b992f
Chris Wright <christopher.wright47@gmail.com>
2013-11-24 15:32:44 -0600

    made tests pass

--- spec/features/tag_features_spec.rb
+++ spec/features/tag_features_spec.rb
@@ -3,15 +3,17 @@ require 'spec_helper'
 feature "Tags" do
 	scenario "can be viewed" do
 		visit '/'
-		click_link "Tags"
+		click_link "tags"
 		expect(page).to have_text("Ruby")
 	end
 
 	scenario "can be used to filter questions" do
 		FactoryGirl.create(:question)
 		visit '/'
-		click_link "Tags"
-		click_link "Ruby"
+		click_link "tags"
+		within('.span3') do
+			click_link "Ruby"
+		end
 		expect(page).to have_text("Question Title")
 	end
 end
